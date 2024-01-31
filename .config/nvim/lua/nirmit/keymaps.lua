1d6ea600bbfe1edcf996767446162836ff31054e
Nirmit <nirmitjoshi.work@gmail.com>
2024-01-31 20:02:42 +0530

    fixed keymap

--- .config/nvim/lua/nirmit/keymaps.lua
+++ .config/nvim/lua/nirmit/keymaps.lua
@@ -5,5 +5,5 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "k", ":m '>-2<CR>gv=gv")
 
 vim.keymap.set("v", "<leader>y", '"+y')
-vim.keymap.set("n", "<leader>yy", 'yy"+')
+vim.keymap.set("n", "<leader>yy", '"+yy')
 vim.keymap.set("n", "<leader>p", '"+p')
