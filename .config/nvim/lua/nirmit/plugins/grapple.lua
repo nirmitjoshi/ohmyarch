e97f1f9d9f05933d9e1333b3ef5a44068edc83e2
Nirmit Joshi <nirmitjoshi.work@gmail.com>
2024-06-23 15:54:22 +0530

    added keymap for 5th tag

--- .config/nvim/lua/nirmit/plugins/grapple.lua
+++ .config/nvim/lua/nirmit/plugins/grapple.lua
@@ -13,6 +13,7 @@ return {
 			{ "<leader>2", "<cmd>Grapple select index=2<cr>",  desc = "Select second tag" },
 			{ "<leader>3", "<cmd>Grapple select index=3<cr>",  desc = "Select third tag" },
 			{ "<leader>4", "<cmd>Grapple select index=4<cr>",  desc = "Select fourth tag" },
+			{ "<leader>5", "<cmd>Grapple select index=5<cr>",  desc = "Select fifth tag" },
 			{ "<C-n>",     "<cmd>Grapple cycle_tags next<cr>", desc = "Go to next tag" },
 			{ "<C-p>",     "<cmd>Grapple cycle_tags prev<cr>", desc = "Go to previous tag" },
 		},
